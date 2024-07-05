b48bb24cceba318a6f0e6de0266e5eb4c0e6cd2c
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-07-05 21:48:44 +0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-export const App = () => {
-  return (
-    <>Hello world</>
-  )
-}
+import {Header} from "./modules/Header/Header";
+import {Footer} from "./modules/Footer/Footer";
+import {Goods} from "./modules/Goods/Goods";
+import {Filter} from "./modules/Filter/Filter";
+import {Hero} from "./modules/Hero/Hero";
+import {Subscribe} from "./modules/Subscribe/Subscribe";
+import {Order} from "./modules/Order/Order";
+
+
+export const App = () => (
+  <>
+    <Header />
+    <main>
+      <Hero />
+      <Filter />
+      <Goods />
+      <Subscribe />
+    </main>
+    <Footer />
+    <Order />
+  </>
+)
